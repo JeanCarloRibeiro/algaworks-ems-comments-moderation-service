68748e789e7428c7d5d780ea3d96a8809f558948
Jean Carlo Ribeiro <jean.ribeiro@valecard.com.br>
2025-05-07 00:50:10 -0300

    Configuracao inicial do projeto com os submodulos

---
+++ src/test/java/com/algacomments/moderation/ModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algacomments.moderation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
